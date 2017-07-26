2a7d2c14f232b6ea21c52a2efb0ec101eaee41bd
Sakina Shivji <shivjisakina@gmail.com>
2017-07-26 17:12:43 -0400

    got composer files and test hello world working

---
+++ tests/index.php
@@ -0,0 +1,18 @@
+<?php require '../vendor/autoload.php'; ?>
+
+    <html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+    <?php echo '<p>Hello World</p>'; ?>
+    </body>
+    </html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sakinashivji
+ * Date: 7/26/17
+ * Time: 5:10 PM
+ */
